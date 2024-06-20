09bc10c6b77ff9401da7f720f1c1967fa6973dda
Kihoon Kwon <kihoonk@amazon.com>
2024-06-21 00:28:46 +0900

    route53 failover record

---
+++ python/route53-failover/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+
+import aws_cdk as cdk
+
+from route53_failover.route53_failover_stack import Route53FailoverStack
+
+
+app = cdk.App()
+Route53FailoverStack(app, 
+                     "Route53FailoverStack",
+                     domain="example.com",
+                     env=cdk.Environment(
+                        account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+                        region=os.getenv('CDK_DEFAULT_REGION')
+                        )
+                    )
+app.synth()
